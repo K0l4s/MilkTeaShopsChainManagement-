00b80d032fcf57d145a3d29683580a92c4b5f4e7
Trung Kien <87256083+K0l4s@users.noreply.github.com>
2023-12-07 13:44:03 +0700

    reset file

--- src/main/java/alotra/milktea/Exception/StorageFileNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package alotra.milktea.Exception;
-
-public class StorageFileNotFoundException  extends StorageException {
-    private static final long serialVersionUID = 1L;
-    public StorageFileNotFoundException(String message) {
-        super(message);
-    }
-}
